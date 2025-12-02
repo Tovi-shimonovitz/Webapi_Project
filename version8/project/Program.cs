80f71f9b53a6efbc17219eced05397aad9fe49bd
הנדסת תוכנה <huser@sem.local>
2025-12-02 17:07:27 +0200

    middlewere

--- version8/project/Program.cs
+++ version8/project/Program.cs
@@ -1,6 +1,6 @@
 using lesson4.interfaces;
 using lesson4.Services;
-
+using LogMiddleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,23 +10,39 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Logging.ClearProviders();//log4net seriLog
+builder.Logging.AddConsole(); 
+
 
 var app = builder.Build();
+
+
+app.UseLogMiddleware();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    //  app.MapOpenApi(); 
+    // app.UseSwaggerUI(options =>
+    // {
+    //     options.SwaggerEndpoint("/openapi/v1.json", "v1");
+    // });
 }
- app.UseDefaultFiles();
-app.UseStaticFiles();
 
+
+
+
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
 
 app.Run();
--- vertion9/Program.cs
+++ vertion9/Program.cs
@@ -9,18 +9,26 @@ builder.Services.AddMusicService();
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+
+    // app.UseSwagger();
+    //  app.UseSwaggerUI();
     app.MapOpenApi(); 
     app.UseSwaggerUI(options =>
     {
         options.SwaggerEndpoint("/openapi/v1.json", "v1");
     });
 }
+
+ app.UseDefaultFiles();
+app.UseStaticFiles();
 
 app.UseHttpsRedirection();
 
